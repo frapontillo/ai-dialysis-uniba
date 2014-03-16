f98096645e691d853d79553f7ec2e98c8101eb26
Francesco Pontillo <francescopontillo@gmail.com>
2014-03-16 23:15:57 +0100

    Add util comments

--- prolog/util.pl
+++ prolog/util.pl
@@ -17,7 +17,9 @@ println(Element) :- not(Element = [Head|Tail]), println([Element]).
 % ---------------- %
 %  STRING CONCAT   %
 % ---------------- %
+% concat_string_list/2 is used to begin the string concatenation
 concat_string_list([Head|Tail], Result) :- concat_string_list([Head|Tail], '', Result).
+% concat_string_list/3 is used to recurse the string concatenation
 concat_string_list([Head|Tail], Temp, Result) :-
 	string_concat(Temp, Head, NewString),
 	concat_string_list(Tail, NewString, Result).
